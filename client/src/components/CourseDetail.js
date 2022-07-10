277f7e9b619c4c770834c62b00bb3253c3775e1c
gurb <Organicmintz@gmail.com>
2022-07-10 16:43:14 -0700

    add proper function handling

--- client/src/components/CourseDetail.js
+++ client/src/components/CourseDetail.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 export function CourseDetail(props){
     const { id } = useParams();
-    // props.whenClicked(`courses/${id}`);
-    // console.log(props.data)
+    useEffect(() =>{
+        props.whenClicked(`courses/${id}`);
+    }, [id])
+    console.log(props.data)
     return(
         <div>
             <h2>This is the page for courses/{id}</h2>
