1f578573c50d42335990db1fa850689a56c6f2d6
gurb <Organicmintz@gmail.com>
2022-07-12 13:58:30 -0700

    remove api request

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
-import React, { useEffect, useState} from 'react'
-import axios from 'axios'
+import React from 'react'
+//import axios from 'axios'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-
+import { CourseProvider } from './Context'
 //CSS
 import './styles/reset.css'
 import './styles/global.css'
@@ -10,38 +10,38 @@ import './styles/global.css'
 //Components
 import { Courses } from './components/Courses';
 import { Header } from './components/Header'
-import { CourseDetail } from './components/CourseDetail'
+//import { CourseDetail } from './components/CourseDetail'
 //import { Home } from './components/Home'
 
 function App() {
 
-  const [ data, setData ] = useState([]);
-  //console.log(res.data.courses)
-  const getData = async(endpoint = "courses") =>{
-    setData([]);
-    console.log(data)
-    const url = `http://localhost:5000/api/${endpoint}`;
-    console.log(url);
-    await axios.get(url)
-      .then(res => setData(res.data.courses))
-      //.then(res => console.log(res.data.courses))
-      .catch(err => console.log(err))
-  }
+  // const [ data, setData ] = useState([]);
+  // //console.log(res.data.courses)
+  // const getData = async(endpoint = "courses") =>{
+  //   setData([]);
+  //   console.log(data)
+  //   const url = `http://localhost:5000/api/${endpoint}`;
+  //   console.log(url);
+  //   await axios.get(url)
+  //     .then(res => setData(res.data.courses))
+  //     //.then(res => console.log(res.data.courses))
+  //     .catch(err => console.log(err))
+  // }
 
-  useEffect(() =>{
-    getData();
-  }, [])
+  // useEffect(() =>{
+  //   getData();
+  // }, [])
 
   return (
-    <Router>
-        <Header />
-        <Routes>
-          {/* <Route path='/' element={<Home />} /> */}
-          <Route path='/' element={<Courses data={data}/>} />
-          <Route path='/courses/:id' element={<CourseDetail whenClicked={getData} data={data}/>} />
-        </Routes>
-    </Router>
-    
+    <CourseProvider>
+      <Router>
+          <Header />
+          <Routes>
+            <Route path='/' element={<Courses />} />
+            {/* <Route path='/courses/:id' element={<CourseDetail />} /> */}
+          </Routes>
+      </Router>
+    </CourseProvider>
   );
 }
 
