07c265d347b0795f8e8ed9c2c396740c8dcc0de2
gurb <Organicmintz@gmail.com>
2022-07-12 15:30:16 -0700

    display basic details for selected courses

--- client/src/components/CourseDetail.js
+++ client/src/components/CourseDetail.js
@@ -1,48 +1,66 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useContext, useState } from "react";
 import { useParams } from "react-router-dom";
+import CourseContext from '../Context';
 
-export function CourseDetail(props){
+/**
+ * * The materials needed are currently all on once list element 
+ * TODO: display materials needed as individual items
+ * TODO: remove extra console logs and remove extra comments
+ */
+
+export function CourseDetail(){
     const { id } = useParams();
+    const { getCourse } = useContext(CourseContext)
+
+    const [ course, setCourse ] = useState([]);
+
     useEffect(() => {
-        props.whenClicked(`courses/${id}`);
-        console.log(props.data[id])
+        getCourse(id)
+            //.then(res => console.log(res.course))
+            .then(res => setCourse(res.course))
+            .catch(err => console.log(err));
+        // const itemsNeeded = course.materialsNeeded.split('\n');
+        // const items = itemsNeeded.map( item => <li>{item}</li>)
+        // console.log(itemsNeeded)
     }, [])
+
+    //course.materialsNeeded.length - 1
     
     return(
-        <div>
-            <h2>This is the page for courses/{id}</h2>
-            <h3>yoyo</h3>
-        </div>
-        // <main>
-        //     <div className="actions--bar">
-        //         <div className="wrap">
-        //             <a className="button" href="create-course.html">Update Course</a>
-        //             <a className="button" href="create-course.html">Delete Course</a>
-        //             <a className="button button-secondary" href="create-course.html">Return to List</a>
-        //         </div>
-        //     </div>
-        //     <div className="wrap">
-        //         <h2>Course Detail</h2>
-        //         <form>
-        //             <div className="main--flex">
-        //                 <div>
-        //                     <h3 className="course--detail--title">Course</h3>
-        //                     <h4 className="course--name">{props.data[id].title}</h4>
-        //                     <p>{props.data[id].owner.firstName}{props.data[id].owner.lastName}</p>
-        //                     <p>{props.data[id].description}</p>
-        //                 </div>
-        //                 <div>
-        //                     <h3 className="course--detail--title">Estimated Time</h3>
-        //                     <p>{props.data[id].estimatedTime}</p>
-        //                     <h3 className="course--detail--title">Materials Needed</h3>
-        //                     <ul className="course--detail--list">
-
-        //                     </ul>
-        //                 </div>
-        //             </div>
-        //         </form>
-        //     </div>
-        // </main>
-        
+        // <div>
+        //     <h2>This is the page for courses/{id}</h2>
+        //     <h3>yoyo</h3>
+        // </div>
+        <main>
+            <div className="actions--bar">
+                <div className="wrap">
+                    <a className="button" href="create-course.html">Update Course</a>
+                    <a className="button" href="create-course.html">Delete Course</a>
+                    <a className="button button-secondary" href="/">Return to List</a>
+                </div>
+            </div>
+            <div className="wrap">
+                <h2>Course Detail</h2>
+                <form>
+                    <div className="main--flex">
+                        <div>
+                            <h3 className="course--detail--title">Course</h3>
+                            <h4 className="course--name">{course.title}</h4>
+                            <p>Billy Bob</p>
+                            <p>{course.description}</p>
+                        </div>
+                        <div>
+                            <h3 className="course--detail--title">Estimated Time</h3>
+                            <p>{course.estimatedTime}</p>
+                            <h3 className="course--detail--title">Materials Needed</h3>
+                            <ul className="course--detail--list">
+                                <li>{course.materialsNeeded}</li>
+                                {/* {items} */}
+                            </ul>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </main>   
     )
 }
