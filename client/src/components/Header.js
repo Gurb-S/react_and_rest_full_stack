56fff0913f7ca8a4ce7c75dd188b01c827915155
gurb <Organicmintz@gmail.com>
2022-07-13 13:40:27 -0700

    add link to signin page

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,14 +2,16 @@ import React from "react";
 
 export function Header() {
     
+    // TODO: add link to sign up page
+    // TODO: make nav switch to signout and username when logged in
     return(
         <header>
             <div className="wrap header--flex">
-                <h1 className="header--logo"><a href="index.html">Courses</a></h1>
+                <h1 className="header--logo"><a href="/">Courses</a></h1>
                 <nav>
                     <ul className="header--signedout">
                         <li><a href="sign-up.html">Sign Up</a></li>
-                        <li><a href="sign-in.html">Sign In</a></li>
+                        <li><a href="/signin">Sign In</a></li>
                     </ul>
                 </nav>
             </div>
