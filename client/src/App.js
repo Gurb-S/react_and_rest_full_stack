974220b2632542c3931589edf9b1a49a70d5d4c4
gurb <Organicmintz@gmail.com>
2022-07-13 13:49:41 -0700

    move file to context folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 //import axios from 'axios'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import { CourseProvider } from './Context'
+import { CourseProvider } from './context/Context'
 
 //CSS
 import './styles/reset.css'
