770dcc951ef9b82b8855faf170388551071cd0c7
gurb <Organicmintz@gmail.com>
2022-07-13 23:09:29 -0700

    delete file

--- client/src/context/authHandler.js
+++
@@ -1,8 +0,0 @@
-import { getUser } from "./Data";
-
-export const signInAuth = async(username, password) => {
-    console.log(username);
-    console.log(password);
-    const user = await getUser(username,password);
-    return user;
-}
