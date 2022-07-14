b202eb57e3a1b6b9066b061550fd9b0ee77f3629
gurb <Organicmintz@gmail.com>
2022-07-13 23:08:11 -0700

    display username when signed in

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,18 +1,33 @@
-import React from "react";
-
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import CourseProvider from "../context/Context";
 export function Header() {
     
-    // TODO: add link to sign up page
+    // // TODO: add link to sign up page
     // TODO: make nav switch to signout and username when logged in
+    // * Link for user signout page has already been added
+
+    const { authenticatedUser } = useContext(CourseProvider);
+
     return(
         <header>
             <div className="wrap header--flex">
                 <h1 className="header--logo"><a href="/">Courses</a></h1>
                 <nav>
-                    <ul className="header--signedout">
-                        <li><a href="sign-up.html">Sign Up</a></li>
-                        <li><a href="/signin">Sign In</a></li>
-                    </ul>
+                    { authenticatedUser ? (
+                        <ul className="header--signedin">
+                            <li>Welcome, {authenticatedUser.firstName} {authenticatedUser.lastName}</li>
+                            <li>
+                                <Link to="/signout">Sign Out</Link>
+                            </li>
+                        </ul>
+                    ) :(
+                        <ul className="header--signedout">
+                            <li><a href="sign-up.html">Sign Up</a></li>
+                            <li><a href="/signin">Sign In</a></li>
+                        </ul>
+                    )}
+
                 </nav>
             </div>
         </header>
