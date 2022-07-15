bd49fa687a69e4d1b7c7f9657839b1fbdaac7be6
Gurb-S <90001188+Gurb-S@users.noreply.github.com>
2022-07-14 19:25:33 -0700

    delete excess component

--- client/src/components/Home.js
+++
@@ -1,9 +0,0 @@
-export function Home(){
-    return(
-        <div>
-            <h1>This is the home page🏠🎆🎆</h1>
-            <a href="http://localhost:3000/courses">Click here to view all courses</a>
-        </div>
-        
-    )
-}
