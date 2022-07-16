0facc19d073c62c66d4e4b4b86448ccf415dcf53
Gurb-S <90001188+Gurb-S@users.noreply.github.com>
2022-07-16 16:12:00 -0700

    delete excess comments

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,6 @@
 import React from 'react'
-//import axios from 'axios'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { CourseProvider } from './context/Context';
-import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 //CSS
@@ -12,33 +10,14 @@ import './styles/global.css'
 
 //Components
 import { Courses } from './components/Courses';
-import { Header } from './components/Header'
-import { CourseDetail } from './components/CourseDetail'
-import { UserSignIn } from './components/UserSignIn'
-import { UserSignOut } from './components/UserSignOut'
-import { PrivateRoute } from './context/PrivateRoute'
+import { Header } from './components/Header';
+import { CourseDetail } from './components/CourseDetail';
+import { UserSignIn } from './components/UserSignIn';
+import { UserSignOut } from './components/UserSignOut';
+import { PrivateRoute } from './context/PrivateRoute';
 import { UpdateCourse } from './components/UpdateCourse';
-//import { Home } from './components/Home'
 
 function App() {
-  // const [ data, setData ] = useState([]);
-  // //console.log(res.data.courses)
-  // const getData = async(endpoint = "courses") =>{
-  //   setData([]);
-  //   console.log(data)
-  //   const url = `http://localhost:5000/api/${endpoint}`;
-  //   console.log(url);
-  //   await axios.get(url)
-  //     .then(res => setData(res.data.courses))
-  //     //.then(res => console.log(res.data.courses))
-  //     .catch(err => console.log(err))
-  // }
-
-  // useEffect(() =>{
-  //   getData();
-  // }, [])
-  
-
   return (
       <CourseProvider>
         <Router>
