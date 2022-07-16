d267b85d1a0d1a6e3f8d942da45f2d3778880c1e
gurb <Organicmintz@gmail.com>
2022-07-16 15:39:00 -0700

    add route to update page

--- client/src/components/CourseDetail.js
+++ client/src/components/CourseDetail.js
@@ -1,16 +1,17 @@
 import React, { useEffect, useContext, useState } from "react";
 import { useParams } from "react-router-dom";
 import CourseContext from '../context/Context';
+import ReactMarkdown from 'react-markdown';
 
 /**
  * * The materials needed are currently all on once list element 
- * TODO: display materials needed as individual items
+ * // TODO: display materials needed as individual items
  * TODO: remove extra console logs and remove extra comments
  */
 
 export function CourseDetail(){
     const { id } = useParams();
-    const { getCourse } = useContext(CourseContext)
+    const { getCourse } = useContext(CourseContext);
 
     const [ course, setCourse ] = useState([]);
 
@@ -34,8 +35,8 @@ export function CourseDetail(){
         <main>
             <div className="actions--bar">
                 <div className="wrap">
-                    <a className="button" href="create-course.html">Update Course</a>
-                    <a className="button" href="create-course.html">Delete Course</a>
+                    <a className="button" href={`/courses/${id}/update`}>Update Course</a>
+                    <a className="button" href='/'>Delete Course</a>
                     <a className="button button-secondary" href="/">Return to List</a>
                 </div>
             </div>
@@ -51,12 +52,13 @@ export function CourseDetail(){
                         </div>
                         <div>
                             <h3 className="course--detail--title">Estimated Time</h3>
-                            <p>{course.estimatedTime}</p>
+                            {course.estimatedTime ?(<p>{course.estimatedTime}</p>) : <ReactMarkdown>*No time was provided by owner*</ReactMarkdown> }
                             <h3 className="course--detail--title">Materials Needed</h3>
-                            <ul className="course--detail--list">
-                                <li>{course.materialsNeeded}</li>
-                                {/* {items} */}
-                            </ul>
+                            {course.materialsNeeded ? (
+                                <ul className="course--detail--list">
+                                    <ReactMarkdown>{course.materialsNeeded}</ReactMarkdown>
+                                </ul>
+                            ) : <ReactMarkdown>*No materials needed was provided by owner*</ReactMarkdown>}
                         </div>
                     </div>
                 </form>
