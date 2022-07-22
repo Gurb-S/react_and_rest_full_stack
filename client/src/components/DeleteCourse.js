bd72bd83948a17e5f68d4cf010897dae7d1c6d1f
gurb <Organicmintz@gmail.com>
2022-07-21 22:47:42 -0700

    check id of corse being deleted

--- client/src/components/DeleteCourse.js
+++ client/src/components/DeleteCourse.js
@@ -4,9 +4,12 @@ import CourseContext from "../context/Context";
 
 export function DeleteCourse() {
     const { deleteCourse } = useContext(CourseContext);
-    //const { id } = useParams();
-    // TODO: update id based on course that linked here
-    deleteCourse(6,'joe@smith.com', 'joepassword');
+    const { id } = useParams();
+    console.log(id)
+    // // TODO: update id based on course that linked here
+    // TODO: passing in user credentials
+    // TODO: check if users deleting is owner of course
+    deleteCourse(id,'joe@smith.com', 'joepassword');
 
     return(
         <Navigate to='/' />
