9c10221da0e34513f1bf4af4ada0982267a60a89
gurb <Organicmintz@gmail.com>
2022-07-22 17:23:55 -0700

    provide proper commenting

--- client/src/context/PrivateRoute.js
+++ client/src/context/PrivateRoute.js
@@ -1,10 +1,16 @@
 import React, { useContext } from "react";
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 import CourseContext from "../context/Context";
 
 
 export const PrivateRoute = ({ children }) => {
+    //import from router dom
+    const location = useLocation();
+
+    //import from context api
     const { authenticatedUser } = useContext(CourseContext);
-    return authenticatedUser ? children : <Navigate to="/signin"/>
+
+    //if user is logged in, they are allowed in the protected route and if not then redirect to login page
+    return authenticatedUser ? children : <Navigate to="/signin" replace state={{ from: location }}/>
 
 }
