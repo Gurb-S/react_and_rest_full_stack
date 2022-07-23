e911bbaa1c0a9c63f3a877ac5766e480b134d8c7
gurb <Organicmintz@gmail.com>
2022-07-22 17:28:19 -0700

    add routes from error routes

--- client/src/App.js
+++ client/src/App.js
@@ -19,6 +19,9 @@ import { UpdateCourse } from './components/UpdateCourse';
 import { UserSignUp } from './components/UserSignUp';
 import { CreateCourse } from './components/CreateCourse';
 import { DeleteCourse } from './components/DeleteCourse';
+import { NotFound } from './components/NotFound';
+import { Forbidden } from './components/Forbidden';
+import { UnhandledError } from './components/UnhandlerError';
 
 function App() {
   return (
@@ -27,13 +30,16 @@ function App() {
           <Header />
           <Routes>
             <Route path='/' element={<Courses />} />
-            <Route path='/courses/:id' element={<PrivateRoute><CourseDetail /></PrivateRoute>} />
+            <Route path='/courses/:id' element={<CourseDetail />} />
             <Route path='/courses/:id/update' element={<PrivateRoute><UpdateCourse /></PrivateRoute>} />
             <Route path='/courses/create' element={<PrivateRoute><CreateCourse /></PrivateRoute>} />
             <Route path='/courses/:id/delete' element={<PrivateRoute><DeleteCourse /></PrivateRoute>}/>
             <Route path='/signin' element={<UserSignIn />} />
             <Route path='/signout' element={<UserSignOut />} />
             <Route path='/signup' element={<UserSignUp />}/>
+            <Route path='/forbidden' element={<Forbidden />}/>
+            <Route path='/error' element={<UnhandledError />}/>
+            <Route path='*' element={<NotFound />}/>
           </Routes>
         </Router>
       </CourseProvider>
